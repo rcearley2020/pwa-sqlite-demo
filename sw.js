06335ee8dc88b8947e6c296b6b60fa9e3bf626ee
rcearley2020 <74525150+rcearley2020@users.noreply.github.com>
2025-08-20 22:35:00 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,23 @@
+const CACHE_NAME = "pwa-sqlite-cache-v1";
+const urlsToCache = [
+  "/pwa-sqlite-demo/",
+  "/pwa-sqlite-demo/index.html",
+  "/pwa-sqlite-demo/app.js",
+  "/pwa-sqlite-demo/icon-192.png",
+  "/pwa-sqlite-demo/icon-512.png",
+  "/pwa-sqlite-demo/manifest.json"
+];
+
+// Install SW and cache files
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./app.js",
-        "./manifest.json",
-        "./icon.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Serve cached files if available
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
